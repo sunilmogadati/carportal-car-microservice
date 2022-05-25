3bea47681f2a8106ab19f8121678144fae0beca9
toiletgod <khayn9ne@gmail.com>
2022-05-25 15:35:30 -0400

    updated fileService code so that the url can be put into the car photo image

--- src/main/java/com/quintrix/carportalcarmicroservice/filestorage/FileServiceImpl.java
+++ src/main/java/com/quintrix/carportalcarmicroservice/filestorage/FileServiceImpl.java
@@ -1,6 +1,7 @@
 package com.quintrix.carportalcarmicroservice.filestorage;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,10 @@ import org.springframework.web.server.ResponseStatusException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.quintrix.carportalcarmicroservice.car.CarEntity;
+import com.quintrix.carportalcarmicroservice.car.CarEntityRepository;
+import com.quintrix.carportalcarmicroservice.car.CarImagesEntity;
+import com.quintrix.carportalcarmicroservice.car.CarImagesRepository;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -18,33 +23,75 @@ public class FileServiceImpl implements FileService {
   @Autowired
   private AmazonS3Client awsS3;
 
+  @Autowired
+  CarEntityRepository carRepo;
+
+  @Autowired
+  CarImagesRepository photoRepo;
+
   @Override
-  public String uploadFile(MultipartFile file) {
+  public String uploadFile(MultipartFile file, String id, int order) {
 
-    // stores the file extension
-    String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+    // gets the car by id
+    Optional<CarEntity> carentity = carRepo.findById(id);
 
-    // create a unique key for the file and stores it
-    String key = UUID.randomUUID().toString() + "." + extension;
 
-    ObjectMetadata metaData = new ObjectMetadata();
-
-    metaData.setContentLength(file.getSize());
-    metaData.setContentType(file.getContentType());
-
-    // Puts File into Amazon S3 Bucket named dw-imagestorage
-    try {
-      awsS3.putObject("dw-imagestorage", key, file.getInputStream(), metaData);
-    } catch (IOException e) {
-      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
-          "Error ocured while uploading the file");
+    // if the car model dont exist do not upload file
+    if (carentity.isEmpty()) {
+      System.out.print("Empty Object");
+      return null;
     }
+    // if model does exist proceed
+    else {
+      System.out.print("Object is not empty");
+      System.out.print(carentity.get().getUuid());
 
-    // makes the file access control public
-    awsS3.setObjectAcl("dw-imagestorage", key, CannedAccessControlList.PublicRead);
 
-    // returns the public url
-    return awsS3.getResourceUrl("dw-imagestorage", key);
+      // System.out.print(carentity.get().getUuid());
+
+      // stores the file extension
+      String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+
+      // create a unique key for the file and stores it
+      String key = UUID.randomUUID().toString() + "." + extension;
+
+      // Create an MetaData Object for the file
+      ObjectMetadata metaData = new ObjectMetadata();
+
+      // gets the file size
+      metaData.setContentLength(file.getSize());
+
+      // gets the content type
+      metaData.setContentType(file.getContentType());
+
+      // Puts File into Amazon S3 Bucket named dw-imagestorage
+      try {
+        awsS3.putObject("dw-imagestorage", key, file.getInputStream(), metaData);
+      } catch (IOException e) {
+        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+            "Error ocured while uploading the file");
+      }
+
+      // makes the file access control public
+      awsS3.setObjectAcl("dw-imagestorage", key, CannedAccessControlList.PublicRead);
+
+      // returns the public url
+      String photoUrl = awsS3.getResourceUrl("dw-imagestorage", key);
+
+      // creates a carimage instance
+      CarImagesEntity image = new CarImagesEntity();
+
+      // add photo url, car id, and dporder to the carimage object
+      image.setAwsS3Url(photoUrl);
+      image.setCarInfoId(id);
+      image.setDisplayOrder(order);
+
+      // saves the image object into the database
+      photoRepo.save(image);
+
+      // returns the public url
+      return photoUrl;
+    }
   }
 
 }
