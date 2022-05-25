cda72018dd8083e44f3eb5bc8c36cb9a45fb0640
toiletgod <khayn9ne@gmail.com>
2022-05-25 03:12:38 -0400

    updated fileService code so that the url can be put into the car photo image also deleted the soap package since its not needed anymore

--- src/main/java/com/quintrix/carportalcarmicroservice/filestorage/FileService.java
+++ src/main/java/com/quintrix/carportalcarmicroservice/filestorage/FileService.java
@@ -3,6 +3,6 @@ package com.quintrix.carportalcarmicroservice.filestorage;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
-  String uploadFile(MultipartFile file);
+  String uploadFile(MultipartFile file, String id);
 
 }
